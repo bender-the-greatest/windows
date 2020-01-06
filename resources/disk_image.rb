a65a24f6ee358c573af910861732084085960942
Alex Alber <metalseargolid@gmail.com>
2020-01-06 14:15:27 -0600

    Added disk_image resource
    Developer Certificate of Origin
    Version 1.1
    
    Copyright (C) 2004, 2006 The Linux Foundation and its contributors.
    1 Letterman Drive
    Suite D4700
    San Francisco, CA, 94129
    
    Everyone is permitted to copy and distribute verbatim copies of this
    license document, but changing it is not allowed.
    
    Developer's Certificate of Origin 1.1
    
    By making a contribution to this project, I certify that:
    
    (a) The contribution was created in whole or in part by me and I
        have the right to submit it under the open source license
        indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the best
        of my knowledge, is covered under an appropriate open source
        license and I have the right under that license to submit that
        work with modifications, whether created in whole or in part
        by me, under the same open source license (unless I am
        permitted to submit under a different license), as indicated
        in the file; or
    
    (c) The contribution was provided directly to me by some other
        person who certified (a), (b) or (c) and I have not modified
        it.
    
    (d) I understand and agree that this project and the contribution
        are public and that a record of the contribution (including all
        personal information I submit with it, including my sign-off) is
        maintained indefinitely and may be redistributed consistent with
        this project or the open source license(s) involved.
    
    Signed-off-by: Alex Alber <metalseargolid@gmail.com>

---
+++ resources/disk_image.rb
@@ -0,0 +1,59 @@
+#
+# Author:: Alex Alber (metalseargolid@gmail.com)
+# Cookbook:: windows
+# Resource:: disk_image
+#
+# Copyright:: 2015-2017, Calastone Ltd.
+# Copyright:: 2018-2019, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+resource_name :windows_disk_image
+
+property :image_path, String, required: true, name_property: true
+
+node.run_state[:mounted_images] = {} if node.run_state[:mounted_images].nil?
+
+action :mount do
+  powershell_script "Mount disk image #{new_resource.image_path}" do
+    code %(Mount-DiskImage '#{new_resource.image_path}' -Confirm:$false -PassThru -EA Stop | Out-Null)
+    guard_interpreter :powershell_script
+    only_if %(( Test-Path -PathType Leaf '#{new_resource.image_path}' ) -And -Not ( Get-DiskImage '#{new_resource.image_path}' ).Attached)
+  end
+
+  ruby_block "Get disk image mountpoint of #{new_resource.image_path}" do
+    block do
+      cmd = %(powershell.exe -Command "( Get-DiskImage '#{new_resource.image_path}' | Get-Volume ).DriveLetter")
+      result = `#{cmd}`
+
+      raise result unless $?.success?
+      node.run_state[:mounted_images][new_resource.image_path] = result.strip
+    end
+    guard_interpreter :powershell_script
+    only_if %(( Test-Path -PathType Leaf '#{new_resource.image_path}' ) -And ( Get-DiskImage '#{new_resource.image_path}' ).Attached)
+  end
+end
+
+action :dismount do
+  powershell_script "Dismount disk image #{new_resource.image_path}" do
+    code <<-EOH
+      while( ( Get-DiskImage '#{new_resource.image_path}' ).Attached ) {
+        Dismount-DiskImage -ImagePath '#{new_resource.image_path}' -Confirm:$false -EA Stop | Out-Null
+      }
+    EOH
+    guard_interpreter :powershell_script
+    only_if %(( Test-Path -PathType Leaf '#{new_resource.image_path}' ) -And ( Get-DiskImage '#{new_resource.image_path}' ).Attached)
+  end
+  node.run_state[:mounted_images].delete(new_resource.image_path)
+end
